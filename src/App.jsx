2fa54ac6e75ea30a01c4caa4bf7a1d62be6f025d
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-02-09 18:49:06 -0300

    req1 - configuração redux e fazendendo tela de login

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Login from './pages/Login';
+
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    );
+  }
+}
+
+export default App;
---
+++ src/redux/actions/player.js
@@ -0,0 +1,6 @@
+import { LOGIN } from './actionTypes';
+
+export const player = (payload) => ({
+  type: LOGIN,
+  payload,
+});

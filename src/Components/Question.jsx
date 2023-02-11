6abef4976c02b983d0a44061447b7971d3fd7912
Fernando Frali <112976898+FernandoFrali@users.noreply.github.com>
2023-02-10 23:52:31 -0300

    req7e8: Agora mostra as respostas corretas e incorretas, além de ser adicionado o timer de 30 segundos

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -2,23 +2,57 @@ import { arrayOf, bool, shape, string, func } from 'prop-types';
 import React, { Component } from 'react';
 
 class Question extends Component {
+  state = {
+    colorCorrect: '',
+    colorIncorrect: '',
+    timer: 30,
+    disabled: false,
+  };
+
+  componentDidMount() {
+    this.startTimer();
+  }
+
+  startTimer = () => {
+    const thousand = 1000;
+    this.timerInterval = setInterval(() => {
+      this.setState((anterior) => {
+        const newTimer = anterior.timer - 1;
+        if (newTimer === 0) {
+          clearInterval(this.timerInterval);
+          return { timer: newTimer, disabled: true };
+        }
+        return { timer: newTimer };
+      });
+    }, thousand);
+  };
+
   questionsShuffle = (arr) => {
     const magic = 0.5;
     return arr.sort(() => Math.random() - magic);
   };
 
-  // handleClick = ({ target }) => {
-  //   console.log(target);
-  // };
+  handleClick = ({ target }) => {
+    if (target.id === 'correct-answer') {
+      this.setState({ colorCorrect: 'green-border', colorIncorrect: 'red-border' });
+    } else {
+      this.setState({ colorIncorrect: 'red-border', colorCorrect: 'green-border' });
+    }
+  };
 
   correctAndIncorrectQuestions = () => {
     const { question } = this.props;
+    const { colorCorrect, colorIncorrect, disabled } = this.state;
     if (question) {
       const correct = (
         <button
           type="button"
           data-testid="correct-answer"
+          id="correct-answer"
           key={ question.correta }
+          onClick={ this.handleClick }
+          className={ colorCorrect }
+          disabled={ disabled }
         >
           { question.correta }
         </button>
@@ -28,7 +62,11 @@ class Question extends Component {
         <button
           type="button"
           data-testid={ `wrong-answer-${index}` }
+          id="wrong-answer"
           key={ answer }
+          onClick={ this.handleClick }
+          className={ colorIncorrect }
+          disabled={ disabled }
         >
           {answer}
         </button>
@@ -42,16 +80,25 @@ class Question extends Component {
   render() {
     const respostas = this.correctAndIncorrectQuestions();
     const { active, categoria, pergunta, onClick } = this.props;
+    const { timer } = this.state;
     if (active === false) return null;
     return (
-      <fieldset name={ pergunta }>
-        <legend data-testid="question-category">{ categoria }</legend>
-        <h2 data-testid="question-text">{ pergunta }</h2>
-        <div data-testid="answer-options">
-          {respostas.map((resposta) => resposta)}
-        </div>
-        <button onClick={ onClick }>Next</button>
-      </fieldset>
+      <div>
+        <p>
+          Time remaining:
+          {timer}
+          {' '}
+          seconds
+        </p>
+        <fieldset name={ pergunta }>
+          <legend data-testid="question-category">{ categoria }</legend>
+          <h2 data-testid="question-text">{ pergunta }</h2>
+          <div data-testid="answer-options">
+            {respostas.map((resposta) => resposta)}
+          </div>
+          <button onClick={ onClick }>Next</button>
+        </fieldset>
+      </div>
     );
   }
 }
